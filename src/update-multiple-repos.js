1f10336d9a1986e6f1221fede75a95a6ca352792
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-12-27 18:56:11 -0500

    parsing config file

--- src/update-multiple-repos.js
+++ src/update-multiple-repos.js
@@ -1,5 +1,5 @@
 require('lazy-ass');
-var check = require('check-types');
+var check = require('check-more-types');
 var stripComments = require('strip-json-comments');
 var fs = require('fs');
 // var nextUpdater = require('./src/next-updater');
@@ -9,9 +9,11 @@ function updateSingleRepo(options) {
   la(check.unemptyString(options.config), 'cannot find config option', options);
   la(fs.existsSync(options.config), 'cannot find file', options.config);
 
-  var config = stripComments(fs.readFileSync(options.config, 'utf-8'));
+  var config = JSON.parse(stripComments(fs.readFileSync(options.config, 'utf-8')));
   console.log('config');
   console.log(config);
+
+  la(check.arrayOfStrings(config.repos), 'cannot find repos list', config);
 }
 
 module.exports = updateSingleRepo;
--- src/update-single-repo.js
+++ src/update-single-repo.js
@@ -2,19 +2,27 @@ require('lazy-ass');
 var check = require('check-types');
 var quote = require('quote');
 var nextUpdater = require('./src/next-updater');
+var q = require('q');
 
 function updateSingleRepo(options) {
   la(check.object(options), 'missing options');
 
+  var defer = q.defer();
+
   nextUpdater.testModuleUpdate(options.repo)
-    .done(function () {
+    .finally(function () {
       console.log('finished testing', quote(options.repo));
     }, function (err) {
       console.error('could not update repo', quote(options.repo));
       console.error('-------------------------');
       console.error(err.stack);
       console.error('-------------------------');
+    })
+    .done(function () {
+      defer.resolve(options.repo);
     });
+
+  return defer.promise;
 }
 
 module.exports = updateSingleRepo;
