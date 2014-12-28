dc8d6f012aa6c192f6dd87d71944ca8a45a843b4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-12-27 23:05:31 -0500

    List of repos updated, fixes #4

--- src/update-multiple-repos.js
+++ src/update-multiple-repos.js
@@ -2,18 +2,33 @@ require('lazy-ass');
 var check = require('check-more-types');
 var stripComments = require('strip-json-comments');
 var fs = require('fs');
-// var nextUpdater = require('./src/next-updater');
+var updateSingleRepo = require('./update-single-repo');
+la(check.fn(updateSingleRepo), 'missing update single repo function', updateSingleRepo);
+var _ = require('lodash');
+var q = require('q');
 
-function updateSingleRepo(options) {
+function updateMultipleRepos(options) {
   la(check.object(options), 'missing options');
   la(check.unemptyString(options.config), 'cannot find config option', options);
   la(fs.existsSync(options.config), 'cannot find file', options.config);
 
   var config = JSON.parse(stripComments(fs.readFileSync(options.config, 'utf-8')));
-  console.log('config');
-  console.log(config);
-
   la(check.arrayOfStrings(config.repos), 'cannot find repos list', config);
+
+  var updates = q();
+  config.repos.forEach(function (repo) {
+    var opts = _.cloneDeep(options);
+    _.defaults(opts, options);
+    opts.repo = repo;
+
+    updates = updates.then(function () {
+      return updateSingleRepo(opts);
+    });
+  });
+
+  updates.done(function () {
+    console.log('all done');
+  });
 }
 
-module.exports = updateSingleRepo;
+module.exports = updateMultipleRepos;
