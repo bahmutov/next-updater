77bba22a43c32d2fd80752f82e86a448ca2c31a3
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-12-29 08:04:21 -0500

    Printing horizontal line between repos, fixes #18

--- src/update-single-repo.js
+++ src/update-single-repo.js
@@ -3,20 +3,23 @@ var check = require('check-types');
 var quote = require('quote');
 var nextUpdater = require('./next-updater');
 var q = require('q');
+var hr = require('hr').hr;
 
 function updateSingleRepo(options) {
   la(check.object(options), 'missing options');
 
   var defer = q.defer();
 
+  hr('=');
+
   nextUpdater.testModuleUpdate(options.repo, options)
     .finally(function () {
       console.log('finished testing', quote(options.repo));
     }, function (err) {
-      console.error('could not update repo', quote(options.repo));
-      console.error('-------------------------');
-      console.error(err.stack);
-      console.error('-------------------------');
+      console.log('could not update repo', quote(options.repo));
+      hr('-');
+      console.log(err.stack);
+      hr('-');
     })
     .done(function () {
       defer.resolve(options.repo);
